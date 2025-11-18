bcb98e11d9c9cbdc3e10830963d249bf4a2ca8a1
Developerzakir <zakir1234@!za>
2025-11-18 10:36:04 +0600

    definition crud completed

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\WordController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\WordController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\DefinitionController;
 
 Route::get('/',[AdminController::class,'login'])->name('admin.login');
 Route::post('/admin/auth',[AdminController::class,'auth'])->name('admin.auth');
@@ -20,6 +21,18 @@ Route::prefix('admin')->middleware('admin')->group(function(){
             'edit'=>'admin.words.edit',
             'update'=>'admin.words.update',
             'destroy'=>'admin.words.destroy'
+        ]
+    ]);
+
+    //definitions routes
+    Route::resource('definitions', DefinitionController::class, [
+        'names' => [
+            'index' => 'admin.definitions.index',
+            'create' => 'admin.definitions.create',
+            'store' => 'admin.definitions.store',
+            'edit' => 'admin.definitions.edit',
+            'update' => 'admin.definitions.update',
+            'destroy' => 'admin.definitions.destroy',
         ]
     ]);
 });
