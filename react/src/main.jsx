71ebd2092f8b856ff9ba6f9cede3229287de8eed
Developerzakir <zakir1234@!za>
2025-11-18 21:48:43 +0600

    React router 7.9.6 setup

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { ToastContainer, toast } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <div className='container'>
+    <ToastContainer position="top-right" />
     <App />
-  </StrictMode>,
+  </div>,
 )
