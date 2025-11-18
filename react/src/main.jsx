058e26b70a734292a90eea5c0c034ca61ad216dd
Developerzakir <zakir1234@!za>
2025-11-18 22:13:09 +0600

    Redux toolkit setup in react project

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -6,9 +6,19 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { ToastContainer, toast } from 'react-toastify';
 
+import { persistor, store } from './redux/store/index.js'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
+
+
 createRoot(document.getElementById('root')).render(
-  <div className='container'>
-    <ToastContainer position="top-right" />
-    <App />
-  </div>,
+    <Provider store={store}>
+    <PersistGate persistor={persistor}> 
+    <div className='container'>
+      <ToastContainer position="top-right" />
+      <App />
+    </div>
+   </PersistGate>
+  </Provider>
+  ,
 )
